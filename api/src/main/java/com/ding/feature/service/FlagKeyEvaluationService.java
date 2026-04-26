5cf641fa63bd30200009abdbd370b5a00d0c0d4f
dhjlxw <394087000@qq.com>
2026-04-26 19:30:50 +0800

    cache

--- api/src/main/java/com/ding/feature/service/FlagKeyEvaluationService.java
+++ api/src/main/java/com/ding/feature/service/FlagKeyEvaluationService.java
@@ -4,12 +4,19 @@ import java.util.List;
 
 import com.ding.feature.beans.FeatureBean;
 import com.ding.feature.beans.FeatureExplainBean;
+import com.ding.feature.beans.FeatureExplainDetailBean;
+
 /*
  * this class should evaluate the given beans asap, the strategy to evaluate is important
  * this class should consider some optimization methods to improve performance
  */
 public interface FlagKeyEvaluationService {
 	boolean evaluate(List<FeatureBean> beans);
+
+	boolean evaluate(FeatureBean bean);
+
 	FeatureExplainBean explain(List<FeatureBean> beans);
+
+	FeatureExplainDetailBean explain(FeatureBean bean);
 
 }
