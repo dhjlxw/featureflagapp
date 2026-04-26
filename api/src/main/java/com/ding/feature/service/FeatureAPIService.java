f8065d04fc00479301af54ffc79020f21d1bd1fd
dhjlxw <394087000@qq.com>
2026-04-26 18:43:20 +0800

    cache

--- api/src/main/java/com/ding/feature/service/FeatureAPIService.java
+++ api/src/main/java/com/ding/feature/service/FeatureAPIService.java
@@ -8,6 +8,7 @@ public interface FeatureAPIService {
 	 * first to check the cache service
 	 * if cache not exists then invoke the remote data service to query and then save to cache
 	 * as the cache is loaded fully before this, so most case the cache should be exists
+	 * when doing evaluation, this method should optimize the memory cache and memory evaluation part to speed up 
 	 */
 	boolean isEnabled(FeatureFlagBean bean);
 }
