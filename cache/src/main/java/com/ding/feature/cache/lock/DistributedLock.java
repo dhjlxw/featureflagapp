8cfbe3b5224cb992f0fa28075dc44ad396c805ce
dhjlxw <394087000@qq.com>
2026-04-26 19:59:22 +0800

    thread

--- cache/src/main/java/com/ding/feature/cache/lock/DistributedLock.java
+++ cache/src/main/java/com/ding/feature/cache/lock/DistributedLock.java
@@ -2,6 +2,7 @@ package com.ding.feature.cache.lock;
 
 /*
  * use redisson lock to automatically renew the expire time
+ * this lock will use threadId to ensure the lock can't be operated by other threads
  */
 public interface DistributedLock {
 	boolean tryLock(String key, long expire);
